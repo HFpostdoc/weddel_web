ddabdde3faf14afa7fee642f146b30493c93c1ff
mklau <matthewklau@fas.harvard.edu>
2016-06-22 16:19:56 -0400

    added sarracenia model

--- src/helpers.R
+++ src/helpers.R
@@ -57,3 +57,16 @@ read.web <- function(file,print.info=TRUE){
 
 ## }
 
+### convert 
+
+edge2flow <- function(x){
+    x[,1:2] <- apply(x[,1:2],2,as.character)
+    x[,3] <- as.numeric(as.character(x[,3]))
+    e.l <- unique(unlist(x[,1:2]))
+    out <- matrix(0,nrow=length(e.l),ncol=length(e.l))
+    rownames(out) <- colnames(out) <- e.l
+    for (i in 1:nrow(x)){
+        out[rownames(out) == x[i,1],colnames(out) == x[i,2]] <- x[i,3]
+    }
+    return(out)
+}
