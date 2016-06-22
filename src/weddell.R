b7c6ff8f6bf35564246bca57aa0f2b7ed2abe99f
mklau <matthewklau@fas.harvard.edu>
2016-06-22 16:02:06 -0400

    added Jacob weddell model

--- src/weddell.R
+++ src/weddell.R
@@ -11,12 +11,39 @@ library(enaR)
 ## read in the data from U of Canberra's format
 weddell <- read.web('../data/WEB342.csv')
 
-## format as a network object
+## read in from EcoArchive
+wed.ea <- readLines('../data/EcoArchive_weddell.csv')
+wed.ea <- lapply(wed.ea,strsplit,split='\t')
+wed.ea <- lapply(wed.ea,unlist)
+head <- c('Food Web',head(wed.ea[[1]],length(wed.ea[[1]])-1))
+wed.ea <- wed.ea[-1]
+wed.ea <- do.call(rbind,wed.ea)
+rown <- wed.ea[,1]
+wed.ea <- wed.ea[,-1]
+colnames(wed.ea) <- head
+head(wed.ea)
 
-wed.web <- pack(weddell)
+wed.com <- unique(c(wed.ea[,'Taxonomy consumer'],wed.ea[,'Taxonomy resource']))
+wed.net <- matrix(0,nrow=length(wed.com),ncol=length(wed.com))
+rownames(wed.net) <- colnames(wed.net) <- wed.com
+
+for (i in 1:nrow(wed.ea)){
+    print(paste(i,'of',nrow(wed.ea),'records'))
+    wed.net[rownames(wed.net) == wed.ea[i,'Taxonomy resource'],colnames(wed.net) == wed.ea[i,'Taxonomy consumer']] <- 1
+}
+
+## format as a network object
+wed.jac <- pack(flow=wed.net) ## Jacob 2011
+wed.hop <- pack(flow=weddell) ## Hopkins 1983
 
 ## conduct analyses
+str.jac <- enaStructure(wed.jac)
+str.hop <- enaStructure(wed.hop)
 
-enaStructure(wed.web)
+str.jac$ns
+str.hop$ns
 
+# Plot networks
 
+plot(wed.jac)
+plot(wed.hop)
